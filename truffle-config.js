74002b9866017568977245f4c18fe04dfd46e225
Sanjay S B <sanjukammath@gmail.com>
2020-01-23 19:23:10 +0530

    initial commit

---
+++ truffle-config.js
@@ -0,0 +1,43 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("Web3");
+const fs = require("fs");
+const path = require("path");
+
+const mnemonicPath = path.resolve(__dirname, "mnemonic");
+const MNEMONIC = fs.readFileSync(mnemonicPath, "utf8");
+
+module.exports = {
+  networks: {
+    development: {
+      provider: function() {
+        return new Web3.providers.WebsocketProvider("ws://localhost:7545");
+      },
+      network_id: "5777"
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          MNEMONIC,
+          "https://ropsten.infura.io/v3/9ec1ba34a9164ea6a05f44a4168aa065"
+        );
+      },
+      network_id: 3,
+      gas: 4000000 //make sure this gas allocation isn't over 4M, which is the max
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(
+          MNEMONIC,
+          "https://rinkeby.infura.io/v3/221cb1c28ce24c269b98142f855c008e"
+        );
+      },
+      network_id: 4,
+      gas: 1000000
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.5.0"
+    }
+  }
+};
